eeed652c9ba0ae484077c9c8306faceccdb0a1d5
s.elamraoui <s.elamraoui@agiradom.com>
2021-02-10 17:26:46 +0100

    liste des films

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import LoginForm from "./components/Login";
 import SignInForm from "./components/signIn";
 import AdminHome from "./components/AdminPages"
 import CreateUser from "./components/Users/CreateUser";
+import ItemList from './components/vitrine/itemList'
+
 
 ReactDOM.render(
     <React.StrictMode>
@@ -19,6 +21,7 @@ ReactDOM.render(
                 <Route exact path="/sign" component={SignInForm}/>
                 <Route exact path="/adminpage" component={AdminHome}/>
                 <Route exact path="/newuser" component={CreateUser}/>
+                <Route exact path="/listitems" component={ItemList}/>
             </div>
         </Router>
     </React.StrictMode>,
