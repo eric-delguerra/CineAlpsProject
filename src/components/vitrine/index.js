41d0e5e16c90b4f6e35bbba1d7968cac3a63c4e1
s.elamraoui <s.elamraoui@agiradom.com>
2021-02-12 09:15:00 +0100

    modif main

--- src/components/vitrine/index.js
+++ src/components/vitrine/index.js
@@ -1,9 +1,7 @@
-/* eslint-disable max-classes-per-file */
-/* eslint-disable react/no-multi-comp */
-
-import { createMedia } from '@artsy/fresnel'
+import {withRouter} from 'react-router-dom';
+import {createMedia} from '@artsy/fresnel'
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import {
     Button,
     Container,
@@ -19,8 +17,10 @@ import {
     Visibility,
 } from 'semantic-ui-react'
 import Footer from '../Footer/Footer'
+import ListItem from '../vitrine/itemList'
+import AdminPage from '../AdminPages/index'
 
-const { MediaContextProvider, Media } = createMedia({
+const {MediaContextProvider, Media} = createMedia({
     breakpoints: {
         mobile: 0,
         tablet: 768,
@@ -28,25 +28,72 @@ const { MediaContextProvider, Media } = createMedia({
     },
 })
 
-/* Heads up!
- * HomepageHeading uses inline styling, however it's not the best practice. Use CSS or styled
- * components for such things.
- */
-
-
-/* Heads up!
- * Neither Semantic UI nor Semantic UI React offer a responsive navbar, however, it can be implemented easily.
- * It can be more complicated, but you can create really flexible markup.
- */
 class DesktopContainer extends Component {
-    state = {}
+    constructor(props) {
+        super(props);
+        this.state = {
+            reglement: false,
+            apropos: true,
+            palmares: false,
+            listFilm: false,
+            admin: false,
+            viewAdmin: false,
+            user: JSON.parse(localStorage.getItem('userInfo'))
+        }
 
-    hideFixedMenu = () => this.setState({ fixed: false })
-    showFixedMenu = () => this.setState({ fixed: false })
+    }
+
+    componentDidMount() {
+        if (this.state.user.result.roleName === 'admin') {
+            this.setState({
+                admin: true,
+                activeItem: 'Admin',
+                apropos: false,
+                reglement: false,
+                palmares: false,
+                listFilm: false,
+                viewAdmin: true
+            })
+        }
+
+    }
+
+    hideFixedMenu = () => this.setState({fixed: false})
+    showFixedMenu = () => this.setState({fixed: false})
+    handleItemClick = (name) => this.setState({activeItem: name})
+    changeItem = (item) => {
+        this.setState({itemSelected: 'A propos'})
+    }
+    onChangeItem = (value) => {
+        console.log(value)
+
+        if (value === 'A propos') {
+            this.setState({apropos: true, reglement: false, palmares: false, listFilm: false, viewAdmin: false})
+        }
+        if (value === 'Le palmarès 2021') {
+            this.setState({apropos: false, reglement: false, palmares: true, listFilm: false, viewAdmin: false})
+        }
+        if (value === 'Les nommés') {
+            this.setState({apropos: false, reglement: false, palmares: false, listFilm: true, viewAdmin: false})
+        }
+        if (value === 'Reglement') {
+            this.setState({apropos: false, reglement: true, palmares: false, listFilm: false, viewAdmin: false})
+        }
+        if (value === 'Admin') {
+            this.setState({apropos: false, reglement: false, palmares: false, listFilm: false, viewAdmin: true})
+        }
+
+    }
+    logout = () => {
+        console.log('logout')
+        window.location.href = "/"
+        localStorage.removeItem('userInfo')
+    }
 
     render() {
-        const { children } = this.props
-        const { fixed } = this.state
+        const {children} = this.props
+        const {fixed, activeItem} = this.state
+        console.log(this.state.user)
 
         return (
             <Media greaterThan='mobile'>
@@ -58,30 +105,71 @@ class DesktopContainer extends Component {
                     <Segment
                         inverted
                         textAlign='center'
-                        style={{ backgroundColor:'#FFFF', padding: '1em 0em' }}
+                        style={{backgroundColor: '#FFFF', padding: '1em 0em'}}
                         vertical
                     >
                         <Menu
                             fixed={fixed ? 'top' : null}
-                            inverted={!fixed}
-                            pointing={!fixed}
-                            secondary={!fixed}
+
                             size='big'
                         >
-                            <Image style={{height:'70px',width:'100px',marginLeft:'15px'}} src={process.env.PUBLIC_URL + "/logo_Multimedialpes.png"} />
+                            <Image style={{height: '70px', width: '100px', marginLeft: '15px'}}
+                                   src={process.env.PUBLIC_URL + "/logo_Multimedialpes.png"}/>
 
-                            <Menu.Item as='a' active>
-                                Home
+                            <Menu.Item as='a' name='A propos' active={activeItem === 'A propos'}
+                                       onClick={() => {
+                                           this.handleItemClick('A propos')
+                                           this.onChangeItem('A propos')
+                                       }}>
+                                A propos
                             </Menu.Item>
-                            <Menu.Item as='a' style={{color:'purple'}} >A propos</Menu.Item>
-                            <Menu.Item as='a' color='purple'>Le palmarès 2021</Menu.Item>
-                            <Menu.Item as='a' color='purple'>Les nommés</Menu.Item>
-                            <Menu.Item as='a' color='purple'>Reglement</Menu.Item>
+                            <Menu.Item
+                                as='a' name='Le palmarès 2021'
+                                active={activeItem === 'Le palmarès 2021'}
+                                onClick={() => {
+                                    this.handleItemClick('Le palmarès 2021')
+                                    this.onChangeItem('Le palmarès 2021')
+                                }}>
+                                Le palmarès 2021
+                            </Menu.Item>
+                            <Menu.Item
+                                as='a' name='Les nommés'
+                                active={activeItem === 'Les nommés'}
+                                onClick={() => {
+                                    this.handleItemClick('Les nommés')
+                                    this.onChangeItem('Les nommés')
+                                }}>
+                                Les nommés
+                            </Menu.Item>
+                            <Menu.Item
+                                as='a'
+                                name='Reglement'
+                                active={activeItem === 'Reglement'}
+                                onClick={() => {
+                                    this.handleItemClick('Reglement')
+                                    this.onChangeItem('Reglement')
+                                }}>
+                                Reglement
+                            </Menu.Item>
+                            {this.state.admin &&
+                            <Menu.Item
+                                as='a'
+                                name='Admin'
+                                active={activeItem === 'Admin'}
+                                onClick={() => {
+                                    this.handleItemClick('Admin')
+                                    this.onChangeItem('Admin')
+                                }}>
+                                Administration
+                            </Menu.Item>
+                            }
+
                             <Menu.Item position='right'>
                                 <Button as='a' inverted={!fixed} color='violet'>
                                     Mon compte
                                 </Button>
-                                <Button as='a' inverted={!fixed} primary={fixed} color='violet' style={{ marginLeft: '0.5em' }}>
+                                <Button as='a' inverted={!fixed} primary={fixed} color='violet'
+                                        style={{marginLeft: '0.5em'}} onClick={() => this.logout()}>
                                     Deconnexion
                                 </Button>
                             </Menu.Item>
@@ -90,8 +178,8 @@ class DesktopContainer extends Component {
                         </Menu>
                     </Segment>
                 </Visibility>
-
-                {children}
+                {this.state.listFilm && <ListItem user={this.state.user.result}/>}
+                {this.state.viewAdmin && <AdminPage/>}
             </Media>
         )
     }
@@ -101,84 +189,15 @@ DesktopContainer.propTypes = {
     children: PropTypes.node,
 }
 
-class MobileContainer extends Component {
-    state = {isIdentidied:false}
 
-    handleSidebarHide = () => this.setState({ sidebarOpened: false })
-
-    handleToggle = () => this.setState({ sidebarOpened: true })
-
-    render() {
-        const { children } = this.props
-        const { sidebarOpened,isIdentidied } = this.state
-
-        return (
-            <Media as={Sidebar.Pushable} at='mobile'>
-                <Sidebar.Pushable>
-                    <Sidebar
-                        as={Menu}
-                        animation='overlay'
-                        inverted
-                        onHide={this.handleSidebarHide}
-                        vertical
-                        visible={sidebarOpened}
-                    >
-                        <Menu.Item as='a' active>
-                            Accueil
-                        </Menu.Item>
-                        <Image style={{height:'80px',width:'100px',margin:'auto'}} src={process.env.PUBLIC_URL + "/logo_Multimedialpes.png"} />
-                        <Menu.Item as='a'>A propos</Menu.Item>
-                        <Menu.Item as='a'>Le palmarès 2020</Menu.Item>
-                        <Menu.Item as='a'>Les nommés</Menu.Item>
-                        <Menu.Item as='a'>Mon profil</Menu.Item>
-                        <Menu.Item as='a'>Déconnexion</Menu.Item>
-                    </Sidebar>
-
-                    <Sidebar.Pusher dimmed={sidebarOpened}>
-                        <Segment
-                            inverted
-                            textAlign='center'
-                            style={{  padding: '1em 0em' }}
-                            vertical
-                        >
-                            <Container>
-                                <Menu inverted pointing secondary size='large'>
-                                    <Menu.Item onClick={this.handleToggle}>
-                                        <Icon name='sidebar' />
-                                    </Menu.Item>
-                                    <Menu.Item position='right'>
-                                        <Button as='a' inverted>
-                                            Log in
-                                        </Button>
-                                        <Button as='a' inverted style={{ marginLeft: '0.5em' }}>
-                                            Sign Up
-                                        </Button>
-                                    </Menu.Item>
-                                </Menu>
-                            </Container>
-
-                        </Segment>
-
-                        {children}
-                    </Sidebar.Pusher>
-                </Sidebar.Pushable>
-            </Media>
-        )
-    }
-}
-
-MobileContainer.propTypes = {
-    children: PropTypes.node,
-}
-
-const ResponsiveContainer = ({ children }) => (
+const ResponsiveContainer = ({children}) => (
     /* Heads up!
      * For large applications it may not be best option to put all page into these containers at
      * they will be rendered twice for SSR.
      */
     <MediaContextProvider>
         <DesktopContainer>{children}</DesktopContainer>
-        <MobileContainer>{children}</MobileContainer>
+
     </MediaContextProvider>
 )
 
@@ -186,62 +205,14 @@ ResponsiveContainer.propTypes = {
     children: PropTypes.node,
 }
 
-const Vitrine = () => (
+const Vitrine = (props) => (
     <ResponsiveContainer>
-        <Segment style={{ padding: '8em 0em', height: '93vh', background: 'linear-gradient( #FF7C6A 50%, #FFFF )'}} vertical>
-            <Grid container stackable verticalAlign='middle'>
-                <Grid.Row>
-                    <Grid.Column floated='right' width={3}>
-
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-        </Segment>
-
-        <Segment style={{ padding: '0em' }} vertical>
-            <Grid celled='internally' columns='equal' stackable>
-                <Grid.Row textAlign='center'>
-                    <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-                        <Header as='h3' style={{ fontSize: '2em' }}>
-                            "What a Company"
-                        </Header>
-                        <p style={{ fontSize: '1.33em' }}>That is what they all say about us</p>
-                    </Grid.Column>
-                    <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-                        <Header as='h3' style={{ fontSize: '2em' }}>
-                            "I shouldn't have gone with their competitor."
-                        </Header>
-                        <p style={{ fontSize: '1.33em' }}>
-                            <Image avatar src='/images/avatar/large/nan.jpg' />
-                            <b>Nan</b> Chief Fun Officer Acme Toys
-                        </p>git add .
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-        </Segment>
-
-        <Segment style={{ padding: '8em 0em' }} vertical>
-            <Container text>
-
-                <Button as='a' size='large'>
-                    Read More
-                </Button>
-
-                <Divider
-                    as='h4'
-                    className='header'
-                    horizontal
-                    style={{ margin: '3em 0em', textTransform: 'uppercase' }}
-                >
-                    <a href='#'>Case Studies</a>
-                </Divider>
-
-
-            </Container>
-        </Segment>
+        <div style={{width: '80%'}}>
+            <ListItem/>
+        </div>
 
         <Footer/>
     </ResponsiveContainer>
 )
 
-export default Vitrine
+export default DesktopContainer
